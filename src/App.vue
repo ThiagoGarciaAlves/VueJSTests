50ae5072915328ed196680ad621843f9ecd494b4
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-06-15 19:33:08 -0300

    Adicionando um card com vue-material

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+
+    <md-card md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Card with hover effect</div>
+          <div class="md-subhead">It also have a ripple</div>
+        </md-card-header>
+
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button @click="test01">Test01</md-button>
+          <md-button @click="test02">Test02</md-button>
+        </md-card-actions>
+      </md-ripple>
+    </md-card>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    test00: function() {
+      console.log("Test00");
+    },
+    test01: function() {
+      console.log("Test01");
+    },
+    test02: function() {
+      console.log("Test02");
+    }
+  }
 }
 </script>
 
